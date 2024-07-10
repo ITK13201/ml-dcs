f32dcc0df0395dc045129d68f7f354276945ed4a
Takumi Ikeda <tkik13201@gmail.com>
2024-07-11 04:58:22 +0900

    modify: removed gpu settings

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,24 +5,20 @@ set -eu
 APP=ml-dcs
 
 usage() {
-    echo "Usage: $0 [-d] [-g] [-q]" 1>&2
+    echo "Usage: $0 [-d] [-q]" 1>&2
     echo "Options: " 1>&2
     echo "-d: Run as development mode" 1>&2
-    echo "-g: Run nvidia smi"
     echo "-q: Quit without running server" 1>&2
     exit 1
 }
 
 ENVIRONMENT=prod
-NVIDIA_SMI=0
 QUIT=0
 
-while getopts :dgqh OPT
+while getopts :dqh OPT
 do
     case $OPT in
     d)  ENVIRONMENT=dev
-        ;;
-    g)  NVIDIA_SMI=1
         ;;
     q)  QUIT=1
         ;;
@@ -37,9 +33,7 @@ if [ "$QUIT" = "1" ]; then
     exit 0
 fi
 
-if [ "$NVIDIA_SMI" = "1" ]; then
-    nvidia-smi
-elif [ "${ENVIRONMENT:-}" = "dev" ]; then
+if [ "${ENVIRONMENT:-}" = "dev" ]; then
     jupyter-lab --ip=0.0.0.0 --port="${PORT:-8000}" --no-browser --NotebookApp.token=''
 elif [ "${ENVIRONMENT:-}" = "prod" ]; then
     python /usr/local/src/"${APP}"/main.py
