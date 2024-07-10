30d1b140e8ff84fda4a9e2b469988e419c9f3fcc
Takumi Ikeda <tkik13201@gmail.com>
2024-07-11 02:41:54 +0900

    modify: enabled docker

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+set -eu
+
+APP=ml-dcs
+
+usage() {
+    echo "Usage: $0 [-d] [-q]" 1>&2
+    echo "Options: " 1>&2
+    echo "-d: Run as development mode" 1>&2
+    echo "-q: Quit without running server" 1>&2
+    exit 1
+}
+
+ENVIRONMENT=prod
+QUIT=0
+
+while getopts :dqh OPT
+do
+    case $OPT in
+    d)  ENVIRONMENT=dev
+        ;;
+    q)  QUIT=1
+        ;;
+    h)  usage
+        ;;
+    \?) usage
+        ;;
+    esac
+done
+
+if [ "$QUIT" = "1" ]; then
+    exit 0
+fi
+
+if [ "${ENVIRONMENT:-}" = "dev" ]; then
+    jupyter-lab --ip=0.0.0.0 --port="${PORT:-8000}" --no-browser --NotebookApp.token=''
+elif [ "${ENVIRONMENT:-}" = "prod" ]; then
+    python /usr/local/src/"${APP}"/main.py
+fi
---
+++ ml-dcs/README.md
@@ -0,0 +1,25 @@
+# For Developers
+
+## Create virtual environment
+
+```shell
+conda env create -n dev -f requirements.yaml
+```
+
+## Activate virtual environment
+
+```shell
+conda activate dev
+```
+
+## Deactivate virtual environment
+
+```shell
+conda deactivate
+```
+
+## Export virtual environment packages
+
+```shell
+conda env export -n dev > requirements.yaml
+```
