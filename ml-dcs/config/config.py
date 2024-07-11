4b65bc10dcc2d020eb36ed501d0768080c8f0e5b
Takumi Ikeda <tkik13201@gmail.com>
2024-07-11 15:26:15 +0900

    add: added logging settings

---
+++ ml-dcs/config/config.py
@@ -0,0 +1,29 @@
+from logging import config
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(levelname)s] %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "verbose",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "__main__": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "": {"level": "INFO", "handlers": ["console"], "propagate": False},
+    },
+}
+
+config.dictConfig(LOGGING)
