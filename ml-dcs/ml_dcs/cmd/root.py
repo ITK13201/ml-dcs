fdef70c71bba3c3da3104116c671b73514a396c4
Takumi Ikeda <tkik13201@gmail.com>
2024-07-18 14:29:07 +0900

    add: added predict_calculation_time command

--- ml-dcs/ml_dcs/cmd/root.py
+++ ml-dcs/ml_dcs/cmd/root.py
@@ -1,5 +1,6 @@
 import argparse
 
+from ml_dcs.cmd.predict_calculation_time import PredictCalculationTimeCommand
 from ml_dcs.cmd.try_sample import TrySampleCommand
 
 
@@ -10,9 +11,16 @@ class RootCommand:
         self.parser = argparse.ArgumentParser(description=RootCommand.description)
         subparsers = self.parser.add_subparsers()
 
+        # try_sample
         parser_try_sample = subparsers.add_parser(TrySampleCommand.name, help=TrySampleCommand.help)
         TrySampleCommand.add_arguments(parser_try_sample)
         parser_try_sample.set_defaults(handler=TrySampleCommand.execute)
+
+        # predict_calculation_time
+        parser_predict_calculation_time = subparsers.add_parser(PredictCalculationTimeCommand.name,
+                                                                help=PredictCalculationTimeCommand.help)
+        PredictCalculationTimeCommand.add_arguments(parser_predict_calculation_time)
+        parser_predict_calculation_time.set_defaults(handler=PredictCalculationTimeCommand.execute)
 
     def execute(self):
         args = self.parser.parse_args()
