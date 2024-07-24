0cfe3afe3607c3f9ea3798d3d1e1c09aca3dbd0d
Takumi Ikeda <tkik13201@gmail.com>
2024-07-25 05:08:04 +0900

    modify: added plt_show method

--- ml-dcs/ml_dcs/internal/ml/prediction_methods.py
+++ ml-dcs/ml_dcs/internal/ml/prediction_methods.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
@@ -20,6 +21,7 @@ class BasePrediction:
         self.y_test: Optional[pd.DataFrame] = None
         self.x_train_std: Optional[pd.DataFrame] = None
         self.x_test_std: Optional[pd.DataFrame] = None
+        self.predicted: Optional[np.ndarray] = None
 
     def _preprocess(self):
         # create train and test data
@@ -33,6 +35,28 @@ class BasePrediction:
         self.x_train_std = scaler.transform(self.x_train)
         self.x_test_std: pd.DataFrame = scaler.transform(self.x_test)
 
+    def predict(self) -> np.ndarray:
+        if self.predicted is None:
+            if hasattr(self, "_create_regression_model"):
+                regressor = self._create_regression_model()
+                self.predicted = regressor.predict(self.x_test_std)
+            else:
+                raise ValueError("Prediction method has not been implemented yet.")
+        return self.predicted
+
+    def evaluate(self) -> MLResult:
+        predicted = self.predict()
+        r2 = r2_score(self.y_test, predicted)
+        mae = mean_absolute_error(self.y_test, predicted)
+        return MLResult(r2_score=r2, mae=mae)
+
+    def plt_show(self):
+        predicted = self.predict()
+        plt.xlabel("predicted")
+        plt.ylabel("actual")
+        plt.scatter(predicted, self.y_test)
+        plt.show()
+
 
 class LinearRegressionPrediction(BasePrediction):
     def _create_regression_model(self) -> LinearRegression:
@@ -40,14 +64,6 @@ class LinearRegressionPrediction(BasePrediction):
         linear_regression = LinearRegression()
         linear_regression.fit(self.x_train_std, self.y_train)
         return linear_regression
-
-    def predict(self) -> MLResult:
-        linear_regression = self._create_regression_model()
-        predicted: np.ndarray = linear_regression.predict(self.x_test_std)
-
-        r2 = r2_score(self.y_test, predicted)
-        mae = mean_absolute_error(self.y_test, predicted)
-        return MLResult(r2_score=r2, mae=mae)
 
 
 class RandomForestPrediction(BasePrediction):
@@ -57,14 +73,6 @@ class RandomForestPrediction(BasePrediction):
         random_forest_regressor.fit(self.x_train_std, self.y_train)
         return random_forest_regressor
 
-    def predict(self) -> MLResult:
-        random_forest_regressor = self._create_regression_model()
-        predicted: np.ndarray = random_forest_regressor.predict(self.x_test_std)
-
-        r2 = r2_score(self.y_test, predicted)
-        mae = mean_absolute_error(self.y_test, predicted)
-        return MLResult(r2_score=r2, mae=mae)
-
 
 class GradientBoostingPrediction(BasePrediction):
     def _create_regression_model(self) -> GradientBoostingRegressor:
@@ -72,11 +80,3 @@ class GradientBoostingPrediction(BasePrediction):
         regressor = GradientBoostingRegressor()
         regressor.fit(self.x_train_std, self.y_train)
         return regressor
-
-    def predict(self) -> MLResult:
-        regressor = self._create_regression_model()
-        predicted: np.ndarray = regressor.predict(self.x_test_std)
-
-        r2 = r2_score(self.y_test, predicted)
-        mae = mean_absolute_error(self.y_test, predicted)
-        return MLResult(r2_score=r2, mae=mae)
