f444cf67bb3c3f0f2176a06a34013bc84efe26e3
Takumi Ikeda <tkik13201@gmail.com>
2025-03-21 04:08:28 +0900

    modify: added training results to unknown-scenario-result

--- ml-dcs/ml_dcs/domain/result.py
+++ ml-dcs/ml_dcs/domain/result.py
@@ -3,8 +3,8 @@ from typing import List
 
 from pydantic import BaseModel, computed_field
 
-from ml_dcs.domain.ml_gnn import GNNTestingResult
-from ml_dcs.domain.ml_simple import MLSimpleTestingResultSet
+from ml_dcs.domain.ml_gnn import GNNTestingResult, GNNTrainingResult
+from ml_dcs.domain.ml_simple import MLSimpleTestingResultSet, MLSimpleTrainingResultSet
 
 
 class UnknownScenarioResult(BaseModel):
@@ -63,7 +63,7 @@ class UnknownScenarioResult(BaseModel):
         else:
             return None
 
-    def update_with_simple(self, input_path: str):
+    def update_with_simple_from_testing(self, input_path: str):
         with open(input_path, "r") as f:
             data_dict = json.load(f)
         data_model = MLSimpleTestingResultSet(**data_dict)
@@ -74,7 +74,7 @@ class UnknownScenarioResult(BaseModel):
             data_model.result_at_best_accuracy.duration.total_seconds() * 1000
         )
 
-    def update_with_gnn(self, input_path: str):
+    def update_with_gnn_from_testing(self, input_path: str):
         with open(input_path, "r") as f:
             data_dict = json.load(f)
         data_model = GNNTestingResult(**data_dict)
@@ -82,3 +82,16 @@ class UnknownScenarioResult(BaseModel):
         self.list_rmse.append(data_model.rmse / 1000)
         self.list_mape.append(data_model.mape)
         self.list_t_pred.append(data_model.duration_avg.total_seconds() * 1000)
+
+    def update_with_simple_from_training(self, input_path: str):
+        with open(input_path, "r") as f:
+            data_dict = json.load(f)
+        data_model = MLSimpleTrainingResultSet(**data_dict)
+        self.list_t_train.append(data_model.duration_avg.total_seconds())
+
+    def update_with_gnn_from_training(self, input_path: str):
+        with open(input_path, "r") as f:
+            data_dict = json.load(f)
+        data_model = GNNTrainingResult(**data_dict)
+        self.list_epochs.append(data_model.epochs)
+        self.list_t_train.append(data_model.duration.total_seconds())
